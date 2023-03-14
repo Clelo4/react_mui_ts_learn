45dc6e735062984637bba32d9b3da87d7320040b
chengjunjie <chengjunjie.jack@gmail.com>
2023-03-15 00:19:54 +0800

    update

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -3,12 +3,13 @@ import Sign from '../pages/sign';
 import ErrorPage from '../pages/errorPage';
 import { Outlet } from 'react-router-dom';import {
   createBrowserRouter,
+  Link,
 } from 'react-router-dom';
 
 export default createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello World!<Outlet /></div>,
+    element: <div>Hello World!<Outlet /><Link to="/login">login</Link>;</div>,
     errorElement: <ErrorPage />,
     children: [
     ],
