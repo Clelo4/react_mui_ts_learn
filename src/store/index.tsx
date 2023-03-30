d970dda80c100d570a6535c12251c370f6e8a911
Clelo4 <admin@mail.chengjunjie.com>
2023-03-30 19:19:42 +0800

    save

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,1 +1,23 @@
-export const store = {};
+import { createStore } from 'redux';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import config from '../config';
+
+interface CounterState {
+  isOpen: any[],
+  defaultId: string,
+  fontFamily: typeof config.fontFamily,
+  borderRadius: typeof config.borderRadius,
+  opened: boolean,
+}
+
+const initialState: CounterState = {
+  isOpen: [], // for active default menu
+  defaultId: 'default',
+  fontFamily: config.fontFamily,
+  borderRadius: config.borderRadius,
+  opened: true
+}
+
+import reducer from './reducer';
+
+export const store = createStore(reducer);
